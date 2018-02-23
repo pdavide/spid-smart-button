9e618dca846d0b620dadd1f9d3f56e50969f3f80
Davide Porrovecchio <pdavide@gmail.com>
2018-02-23 02:34:46 +0100

    Updated content and style of modal panels

--- src/js/agid-spid-enter-tpl.js
+++ src/js/agid-spid-enter-tpl.js
@@ -134,32 +134,44 @@ window.AgidSpidEnter.prototype.tpl = {
     // Fake content, Lipsum HTML, rimpiazzare con contenuti/etichette reali
     nonHaiSpid: function () {
         return [
-            '<h1 id="agid-infomodal-title">Non hai Spid?</h1>',
-            '<ul>',
-                '<li>Morbi in sem quis dui placerat ornare. Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu. Cras consequat.</li>',
-                '<li>Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus.</li>',
-                '<li>Phasellus ultrices nulla quis nibh. Quisque a lectus. Donec consectetuer ligula vulputate sem tristique cursus. Nam nulla quam, gravida non, commodo a, sodales sit amet, nisi.</li>',
-                '<li>Pellentesque fermentum dolor. Aliquam quam lectus, facilisis auctor, ultrices ut, elementum vulputate, nunc.</li>',
-            '</ul>'
+            '<div class="Prose">',
+				'<h2 class="u-text-h2 u-margin-bottom-xxs">Richiedi SPID</h2>',
+                '<p>',
+                    'Per richiedere e ottenere le tue credenziali SPID, il Sistema Pubblico di Identità Digitale, devi esser maggiorenne.',
+                '</p>',
+                '<h3 class="u-text-h3">Di cosa hai bisogno</h3>',
+                '<ul>',
+                    '<li>un indirizzo e-mail</li>',
+                    '<li>il numero di telefono del cellulare che usi normalmente</li>',
+                    '<li>un documento di identità valido (uno tra: carta di identità, passaporto, patente, permesso di soggiorno)*</li>',
+                    '<li>la tua tessera sanitaria con il codice fiscale*</li>',
+                '</ul>',
+                '<p class="u-textSecondary"><small>',
+                    '* Durante la registrazione può esser necessario fotografarli e allegarli al form che compilerai.',
+                '</small></p>',
+				'<p class="Prose">',
+                    '<a role="button" class="Button Button--default Button--shadow Button--round u-text-r-xs" href="https://www.spid.gov.it/richiedi-spid" target="_blank">',
+                       'Richiedi SPID',
+                    '</a>',
+				'</p>',
+			'</div>'
         ].join('');
     },
 
     // Fake content, Lipsum HTML, rimpiazzare con contenuti/etichette reali
     cosaSpid: function () {
         return [
-            '<h1 id="agid-infomodal-title">Cosa è SPID</h1>',
-            '<p><strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. <a href="#">Donec non enim</a> in turpis pulvinar facilisis. Ut felis.</p>',
-            '<h2>Header Level 2</h2>',
-            '<ol>',
-                '<li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>',
-                '<li>Aliquam tincidunt mauris eu risus.</li>',
-            '</ol>',
-            '<blockquote><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus magna. Cras in mi at felis aliquet congue. Ut a est eget ligula molestie gravida. Curabitur massa. Donec eleifend, libero at sagittis mollis, tellus est malesuada tellus, at luctus turpis elit sit amet quam. Vivamus pretium ornare est.</p></blockquote>',
-            '<h3>Header Level 3</h3>',
-            '<ul>',
-                '<li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>',
-                '<li>Aliquam tincidunt mauris eu risus.</li>',
-            '</ul>'
+            '<div class="Prose">',
+				'<h2 class="u-text-h2 u-margin-bottom-xs">SPID</h2>',
+				'<p class="u-text-r-m u-lineHeight-l u-textSecondary">',
+					"SPID, il Sistema Pubblico di Identità Digitale, è la soluzione che ti permette di accedere a tutti i servizi online della Pubblica Amministrazione con un'unica Identità Digitale (username e password) utilizzabile da computer, tablet e smartphone.",
+				'</p>',
+				'<p class="Prose">',
+					'<a role="button" class="Button Button--default Button--shadow Button--round u-text-r-xs" href="https://www.spid.gov.it/richiedi-spid" target="_blank">',
+	                   'Richiedi SPID',
+					'</a>',
+				'</p>',
+			'</div>'
         ].join('');
     }
 };
